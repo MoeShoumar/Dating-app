51eacc1170feff1636b7db32a34516ffc4a3e121
MoeShoumar <mks38@mail.aub.edu>
2023-03-17 16:37:37 +0200

    added auth and user routes

--- back-end/laravel/routes/api.php
+++ back-end/laravel/routes/api.php
@@ -25,18 +25,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return json_encode($x);
 // });
 
-Route::post('/login', [TestController::class, "login"]);
-
-Route::group(['prefix' => 'admin'], function () {
-    Route::get('/user', [AdminController::class, "users"]);
-    Route::post('/adduser', [AdminController::class, "adduser"]);
-});
-
-
 Route::group(['prefix' => 'v0.0.1'], function () {
-    Route::get('/user', [AdminController::class, "users"]);
-    Route::post('/adduser', [AdminController::class, "adduser"]);
+    Route::group(['prefix' => 'auth'], function () {
+        Route::post('/signin', [AdminController::class, "signin"]);
+        Route::post('/signup', [AdminController::class, "signup"]);
+        Route::post('/resetpass', [AdminController::class, "resetpass"]);
+    });
+    
+    Route::group(['prefix' => 'user'], function () {
+        Route::get('/user', [AdminController::class, "user"]);
+        Route::get('/user', [AdminController::class, "user"]);
+
+        Route::post('/adduser', [AdminController::class, "adduser"]);
+    
+    
+    });
+
+
+   
 });
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
