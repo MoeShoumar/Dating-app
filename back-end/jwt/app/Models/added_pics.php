258ba81fed1d472aac90470d794fb713dbd72d4b
MoeShoumar <mks38@mail.aub.edu>
2023-03-18 08:08:29 +0200

    created models for all tables

---
+++ back-end/jwt/app/Models/added_pics.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class added_pics extends Model
+{
+    use HasFactory;
+}
