5765e3701a518e3710590617a2ea6a08f535343d
MoeShoumar <mks38@mail.aub.edu>
2023-03-18 08:08:53 +0200

    added and finalized routes

--- back-end/jwt/routes/api.php
+++ back-end/jwt/routes/api.php
@@ -8,17 +8,26 @@ Route::group(['middleware' => 'api'], function($router) {
     Route::group(['prefix' => 'v0.0.1'], function () {
         
         Route::group(['prefix' => 'auth'], function () {
-            Route::post('/signin', [JWTController::class, "login"]);
-            Route::post('/signup', [JWTController::class, "register"]);
+            Route::post('/login', [JWTController::class, "login"]);
+            Route::post('/register', [JWTController::class, "register"]);
             Route::post('/resetpass', [JWTController::class, "resetpass"]);
         });
         
         Route::group(['prefix' => 'user'], function () {
-            Route::get('/user', [JWTController::class, "user"]);
+            Route::get('/user/{id}', [JWTController::class, "getuser"]);
+            Route::get('/users', [JWTController::class, "getusers"]);
             Route::get('/likeuser', [JWTController::class, "likeuser"]);
             Route::get('/blockuser', [JWTController::class, "blockuser"]);
             Route::get('/sendmessage', [JWTController::class, "sendmessage"]);
-            Route::post('/adduser', [JWTController::class, "adduser"]);
+            Route::get('/addimage', [JWTController::class, "addimage"]);
+
+            
+        });
+
+        Route::group(['prefix' => 'msgs'], function () {
+            Route::post('/messages', [JWTController::class, "getmessage"]);
+            Route::post('/blocks', [JWTController::class, "getblocks"]);
+            Route::post('/favorites', [JWTController::class, "getfavorites"]);
         });
     
 });
