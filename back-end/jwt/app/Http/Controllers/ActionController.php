f59e13b35948d0efd82896cbe78fc4bfd37f2541
MoeShoumar <mks38@mail.aub.edu>
2023-03-18 14:43:17 +0200

    updated favorites api

--- back-end/jwt/app/Http/Controllers/ActionController.php
+++ back-end/jwt/app/Http/Controllers/ActionController.php
@@ -12,7 +12,7 @@ use App\Models\added_pics;
 
 class ActionController extends Controller
 {
-  
+   // send message function
     function sendmessage(Request $request, $sender_id,$receiver_id){
     $sender = User::find($sender_id);
     $receiver= User::find($receiver_id);
@@ -26,19 +26,29 @@ class ActionController extends Controller
     $message->save();
     return response()->json(['message'=>'message sent succefully']);
   }  
+
+  // Like and dislike function
   function likeuser(Request $request, $sender_id,$receiver_id){
     $sender = User::find($sender_id);
     $receiver= User::find($receiver_id);
     if (!$sender||!$receiver ){
         return response()->json(['message' => 'Invalid sender or receiver ID']);
     }
+    $like= favorites::where('receiver_id', $receiver_id)->where('sender_id', $sender_id)->first();
+    if ($like){
+      $like->delete();
+        return response()->json(['message'=>'user removed from favorites']);
+    }
+    else{
     $like = new favorites;
     $like->receiver_id = $receiver_id;
     $like->sender_id = $sender_id;
     $like->save();
     return response()->json(['message'=>'user added to favorites']);
+  }
 
   }
+   // block function
   function blockuser(Request $request, $sender_id,$receiver_id){
     $sender = User::find($sender_id);
     $receiver= User::find($receiver_id);
