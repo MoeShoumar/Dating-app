15a58071a526057e31febd198eeb3e992232833a
MoeShoumar <mks38@mail.aub.edu>
2023-03-19 17:18:44 +0200

    added userlist html and linked it

--- frontend/index.js
+++ frontend/index.js
@@ -13,7 +13,7 @@ dating_pages.getAPI = async (api_url, api_token = null) => {
     console.log("Error from GET API");
   }
 };
-console.log(typeof dating_pages.getAPI);
+
 dating_pages.postAPI = async (api_url, api_data, api_token = null) => {
   try {
     return await axios.post(api_url, api_data, {
@@ -48,10 +48,10 @@ dating_pages.load_signin = async () => {
     const data = { email: email, password: password };
     const signin_url = `${dating_pages.base_url}auth/login`;
     const response = await dating_pages.postAPI(signin_url, data);
-    console.log(response.data);
+    console.log(response.data.access_token);
     console.log(response.status);
     if (response.status == "200") {
-      localStorage.setItem("jwt", response.data);
+      localStorage.setItem("jwt", response.data.access_token);
       window.location.href = "users-list.html";
     } else {
       const error_message = document.getElementById("error_message");
@@ -104,45 +104,51 @@ dating_pages.load_signup = async () => {
   });
 };
 // upload images
-dating_pages.load_profile = async () => {
-  const mainPic = document.getElementById("profile_main");
-  const optionalCards = document.querySelectorAll(".optional .card");
-  mainPic.addEventListener("click", () => {
-    const imageInput = document.createElement("input");
-    imageInput.type = "file";
-    imageInput.accept = "image/*";
-    const reader = new FileReader();
-    let encoded;
-    imageInput.addEventListener("change", (event) => {
-      reader.readAsDataURL(event.target.files[0]);
-      reader.addEventListener("load", () => {
-        mainPic.querySelector("img").src = reader.result;
-        encoded = reader.result;
-      });
-    });
-    imageInput.click();
-  });
+// dating_pages.load_profile = async () => {
+//   const mainPic = document.getElementById("profile_main");
+//   const optionalCards = document.querySelectorAll(".optional .card");
+//   mainPic.addEventListener("click", () => {
+//     const imageInput = document.createElement("input");
+//     imageInput.type = "file";
+//     imageInput.accept = "image/*";
+//     const reader = new FileReader();
+//     let encoded;
+//     imageInput.addEventListener("change", (event) => {
+//       reader.readAsDataURL(event.target.files[0]);
+//       reader.addEventListener("load", () => {
+//         mainPic.querySelector("img").src = reader.result;
+//         encoded = reader.result;
+//       });
+//     });
+//     imageInput.click();
+//   });
 
-  optionalCards.forEach((card) => {
-    optionalCards.addEventListener("click", () => {
-      const imageInput = document.createElement("input");
-      imageInput.type = "file";
-      imageInput.accept = "image/*";
-      imageInput.addEventListener("change", (event) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(event.target.files[0]);
-        reader.onload = () => {
-          card.querySelector("img").src = reader.result;
-        };
-      });
-      imageInput.click();
-    });
+//   optionalCards.forEach((card) => {
+//     optionalCards.addEventListener("click", () => {
+//       const imageInput = document.createElement("input");
+//       imageInput.type = "file";
+//       imageInput.accept = "image/*";
+//       imageInput.addEventListener("change", (event) => {
+//         const reader = new FileReader();
+//         reader.readAsDataURL(event.target.files[0]);
+//         reader.onload = () => {
+//           card.querySelector("img").src = reader.result;
+//         };
+//       });
+//       imageInput.click();
+//     });
 
-    upload_btn.addEventListener("submit", async (event) => {
-      event.preventDefault();
-      const body = new FormData();
-      body.append("encoded", encoded.split(",")[1]);
-    });
-  });
+//     upload_btn.addEventListener("submit", async (event) => {
+//       event.preventDefault();
+//       const body = new FormData();
+//       body.append("encoded", encoded.split(",")[1]);
+//     });
+//   });
+// };
+
+// userupload
+dating_pages.load_users_list = async () => {
+  console.log(hello);
 };
+
 // localStorage.setItem("jwt", response_signup.data.token);
