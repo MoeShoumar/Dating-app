c58c83fac1b28a055b228416904d8a5fe8179be7
MoeShoumar <mks38@mail.aub.edu>
2023-03-22 07:13:27 +0200

    adjusted the favorites model

--- back-end/jwt/app/Models/favorites.php
+++ back-end/jwt/app/Models/favorites.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\User;
 
 class favorites extends Model
 {
     use HasFactory;
     protected $fillable = ['pic1','pic2','pic3'];
+
+    public function liked(){
+        return $this->belongsTo(User::class, "receiver_id");
+    }
 }
